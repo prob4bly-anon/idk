913c1ea390c95204d93b7624f5a8ac246a39a4c8
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-02-21 23:53:54 +0530

    Create cookie.js

---
+++ docs/js/cookie.js
@@ -0,0 +1,43 @@
+
+function setCookie(cname,cvalue,exdays) {
+  const d = new Date();
+  d.setTime(d.getTime() + (exdays*24*60*60*1000));
+  let expires = "expires=" + d.toUTCString();
+  document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function getCookie(cname) {
+  let name = cname + "=";
+  let decodedCookie = decodeURIComponent(document.cookie);
+  let ca = decodedCookie.split(';');
+  for(let i = 0; i < ca.length; i++) {
+    let c = ca[i];
+    while (c.charAt(0) == ' ') {
+      c = c.substring(1);
+    }
+    if (c.indexOf(name) == 0) {
+      return c.substring(name.length, c.length);
+    }
+  }
+  return "";
+}
+
+function checkCookie() {
+  let user = getCookie("username");
+  if (user != "") {
+    window.location="ODg4NDQ3ODgyNzg.html"
+  } else {
+     user = prompt("Please enter password:","");
+     if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+  if(window.location==="ODg4NDQ3ODgyNzg.html"){
+    user = prompt("Please enter password:","");
+    if (user != "" && user != null) {
+       setCookie("username", user, 30);
+     }
+    }
+  }
+}
+
+
