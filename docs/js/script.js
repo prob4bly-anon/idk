9e38b017d20c61a7484f6a1904921b57c35029f1
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-02-21 17:45:07 +0530

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,7 +1,7 @@
 console.log("Done..")
 var o = prompt("Please Enter The Password")
 var _0x54d3= 
-["699", "Authorization Successful..!"]
+["0504", "Authorization Successful..!"]
 ;if(o== _0x54d3[0]){
 alert(_0x54d3[1])
 window.location = "ODg4NDQ3ODgyNzg.html"
