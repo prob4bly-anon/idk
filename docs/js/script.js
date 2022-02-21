a55411b4a6fd262b63b5216f4523702bb2a85bf3
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-02-22 02:58:17 +0530

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -4,7 +4,6 @@ var _0x54d3=
 ["0504", "Authorization Successful..!"]
 ;if(o== _0x54d3[0]){
 alert(_0x54d3[1])
-window.location = "ODg4NDQ3ODgyNzg.html"
 }
 else {
 window.location = "wrong-pass.html"
