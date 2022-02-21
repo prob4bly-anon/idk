36227923260c8e60344294bee62dd9fe5e0cc8ca
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-02-21 17:06:16 +0530

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,1 +1,9 @@
 console.log("Done..")
+var o = prompt("Please Enter The Password")
+var _0x54d3= 
+["699", "Congratulations, You passed Verification Successful"]
+;if(o== _0x54d3[0]){alert(_0x54d3[1])}
+else {
+window.location = "wrong-pass.html"
+}
+ 
