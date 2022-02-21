267641624aa3add07c479845a31e2427e53aca8c
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-02-21 17:23:08 +0530

    Update script.js

--- docs/js/script.js
+++ docs/js/script.js
@@ -1,8 +1,11 @@
 console.log("Done..")
 var o = prompt("Please Enter The Password")
 var _0x54d3= 
-["699", "Congratulations, You passed Verification Successful"]
-;if(o== _0x54d3[0]){alert(_0x54d3[1])}
+["699", "Authorization Successful..!"]
+;if(o== _0x54d3[0]){
+alert(_0x54d3[1])
+window.location = "ritu.html"
+}
 else {
 window.location = "wrong-pass.html"
 }
