845faba33935a23751d6bafc09b5a72e591ce2bb
Prob4bly_Anon <76247497+prob4bly-anon@users.noreply.github.com>
2022-03-28 00:21:30 +0530

    Delete index.js

--- docs/index.js
+++
@@ -1,9 +0,0 @@
-var xhr = new XMLHttpRequest();
-
-xhr.onload = function (req, res) {
-  // Request finished. Do processing here.
-res.send("ok")
-};
-xhr.open('GET', https://prob4bly-anon.tk/test.html, true);
-xhr.send('');
-
